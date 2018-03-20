b8dd57e9916508a37c88b48eb0a88a89b2db81a6
pf <starzed@yandex.ru>
2018-03-20 17:28:36 +0300

    beautify output: hide unused params, offset for comment block

--- outputMethods.js
+++ outputMethods.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var JSON5 = require('json5-utils')
+
 function compareKeyframes(a, b) {
   return a.key - b.key;
 }
@@ -17,34 +19,81 @@ function getViewKeyframe(keyframe){
   return `${seconds}:${frames}`;
 }
 
+var DEFAULTS = {
+  rotation:0,
+  alpha:1,
+  regX:0, regY:0,
+  x:0, y:0,
+  scaleX:1, scaleY:1,
+}
+
+function clearUnusedParams(keys){
+  var temp = JSON.parse(JSON.stringify(DEFAULTS));
+  var paramNames = Object.keys(DEFAULTS);
+  var unused = [];
+  var used = [];
+  var ret = [];
+
+  var prevTf = JSON.parse(JSON.stringify(DEFAULTS));
+  for(var j=0;j<keys.length;j++) {
+    var newKey = {};
+    var tf = keys[j].tf;
+    for(var p=0;p<paramNames.length;p++) {
+      var param = paramNames[p];
+      if(tf[param] === DEFAULTS[param]){
+        used.push(param);
+      }
+      if(tf[param] !== prevTf[param]){
+        newKey[param] = tf[param];
+      }
+    }
+    ret.push(newKey);
+    prevTf = tf;
+  }
+  return ret;
+}
+
+function getFormatedParams(params){
+  return JSON5.stringify(params, '', '');
+}
+
 // output like createjs animation definition.
 function cjs(aeJSON){
   var str = '';
 	var layers = aeJSON.layers;
 	for (var i=0;i<aeJSON.layers.length;i++){
     var layer = _tr2(aeJSON.layers[i]);
-    //var layer = aeJSON.layers[i];
 		var name = layer.name;
     var index = layer.index;
 		var keys = layer.keys;
 		var oldKeys = layer.oldKeys;
-		var absKey = 0;
     var prevKey = keys[0].key;
+    var clearedParams = clearUnusedParams(keys);
 
     str += `${index} ${name}\ncreatejs.Tween.get( this._fContainer_cjc, {useTicks:true})\n`;
 
+    // for beautyful output
+    var stringParts = [];
+    var maxBaseLength = 0;
 		for(var j=0;j<keys.length;j++) {
-			var isLastKey = j===keys.length-1;
-      var ending = isLastKey ? '' : '\n';
-      var paramsString = JSON.stringify(keys[j].tf);
+      var ending = (j===keys.length-1) ? '' : '\n';
+      var paramsString = getFormatedParams(clearedParams[j]);
       var keyframe = keys[j].key;
       var duration = (keyframe-prevKey);
-      var viewKeyframe = `${keyframe}\t${getViewKeyframe(prevKey)}->${getViewKeyframe(keyframe)}`;
-      str += `\t.to(${paramsString}, ${duration*2}) // ${viewKeyframe}${ending}`;
-			absKey += duration;
+
+      var viewKeyframe = ` // ${prevKey}-${keyframe}\t(${getViewKeyframe(prevKey)}->${getViewKeyframe(keyframe)})`;
+      var viewBase = `\t.to(${paramsString}, ${duration*2})`;
+
+      stringParts.push([viewBase, viewKeyframe]);
+      maxBaseLength = Math.max(viewBase.length, maxBaseLength);
       prevKey = keyframe;
 		}
-		str += '\n\n';
+
+    stringParts = stringParts.map(function(it){
+      return `${it[0].padEnd(maxBaseLength)}${it[1]}`;
+    });
+    console.log(stringParts);
+		str += stringParts.join('\n') + '\n\n';
 	}
 	str += '\n';
 
